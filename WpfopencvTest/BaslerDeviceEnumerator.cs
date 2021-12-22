5bde4c988e9fabba3d1731f91bce6eb99469a320
Thong <thongchai_12889@hotmail.com>
2021-12-22 14:55:23 +0900

    Basler test

---
+++ WpfopencvTest/BaslerDeviceEnumerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Basler.Pylon;
+
+namespace WpfopencvTest
+{
+    public class BaslerDevice
+    {
+        public string Name { get; set; }
+        public string Serial { get; set; }
+    }
+    public static class BaslerDeviceEnumerator
+    {
+        public static List<BaslerDevice> DeviceConnectList()
+        {
+            List<ICameraInfo> allCameras = CameraFinder.Enumerate();
+            var listCam = new List<BaslerDevice>();
+
+            foreach (ICameraInfo cameraInfo in allCameras)
+            {
+                bool newitem = true;
+                foreach (var list in listCam)
+                {
+                    ICameraInfo tag = list as ICameraInfo;
+
+                    // Is the camera found already in the list of cameras?
+                    if (tag[CameraInfoKey.FullName] == cameraInfo[CameraInfoKey.FullName])
+                    {
+                        tag = cameraInfo;
+                        newitem = false;
+                        break;
+                    }
+
+                }
+                if (newitem)
+                {
+                    listCam.Add(new BaslerDevice() { Name = cameraInfo[CameraInfoKey.FriendlyName], Serial = cameraInfo[CameraInfoKey.SerialNumber] });
+
+                }
+            }
+
+            return listCam;
+        }
+    }
+}
--- WpfopencvTest/MainWindow.xaml.cs
+++ WpfopencvTest/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using OpenCvSharp;
+using Basler.Pylon;
+using System.Threading;
+using System.Drawing;
+using System.IO;
 
 namespace WpfopencvTest
 {
@@ -22,23 +26,28 @@ namespace WpfopencvTest
     public partial class MainWindow : System.Windows.Window
     {
         private bool IsLoop = false;
+        private Camera camera = null;
+        private bool grabbing = false;
+        private PixelDataConverter converter = new PixelDataConverter();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        private void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            IsLoop = await StartRollingCamera(0);
-            ImageData.Source = null;
+            //IsLoop = await StartRollingCamera(0);
+            //ImageData.Source = null;
+            conShot();
         }
 
         private void btnFinifh_Click(object sender, RoutedEventArgs e)
         {
-            IsLoop = false;
+            grabbing = false;
+            //IsLoop = false;
         }
 
-        private async Task<bool> StartRollingCamera(int index)
+        /*private async Task<bool> StartRollingCamera(int index)
         {
             //カメラ画像取得用のVideoCaptureのインスタンス生成
             var capture = new VideoCapture(index);
@@ -67,6 +76,101 @@ namespace WpfopencvTest
                 });
             }
             return false;
+        }*/
+        private void CamInit()
+        {
+            var baslerSerial = BaslerDeviceEnumerator.DeviceConnectList()[0].Serial;
+
+            // Create a new camera object.
+            camera = new Camera(baslerSerial);
+
+            grabbing = false;
+        }
+        private void conShot()
+        {
+            if (!grabbing)
+            {
+                grabbing = true;
+
+                try
+                {
+                    Thread thread = new Thread(() => th_grab(0));
+                    thread.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+        private void th_grab(int snap_wait = 500)
+        {
+            try
+            {
+                // Set the acquisition mode to free running continuous acquisition when the camera is opened.
+                camera.CameraOpened += Configuration.AcquireContinuous;
+
+                // Open the connection to the camera device.
+                camera.Open();
+
+                camera.StreamGrabber.Start();
+
+                while (grabbing)
+                {
+                    IGrabResult grabResult = camera.StreamGrabber.RetrieveResult(5000, TimeoutHandling.ThrowException);
+
+                    using (grabResult)
+                    {
+                        if (grabResult.GrabSucceeded)
+                        {
+                            Mat img = convertIImage2Mat(grabResult);
+
+                            Bitmap bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(img);
+                            /*Dispatcher.BeginInvoke((Action)(() =>
+                            {
+                                using(MemoryStream memory = new MemoryStream())
+                                {
+                                    bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                                    memory.Position = 0;
+                                    BitmapImage bitmapimage = new BitmapImage();
+                                    bitmapimage.BeginInit();
+                                    bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                                    bitmapimage.StreamSource = memory;
+                                    bitmapimage.EndInit();
+
+                                    ImageData.Source = bitmapimage;
+                                }
+                            }));*/
+
+                            Dispatcher.Invoke(() => ImageData.Source = OpenCvSharp.WpfExtensions.BitmapSourceConverter.ToBitmapSource(img));
+                        }
+                    }
+
+                    Thread.Sleep(snap_wait);
+                }
+
+                camera.StreamGrabber.Stop();
+                camera.Close();
+            }
+            catch(Exception exception)
+            {
+                if (camera.IsOpen)
+                    camera.Close();
+
+                MessageBox.Show("Exception: {0}" + exception.Message);
+            }
+        }
+        private Mat convertIImage2Mat(IGrabResult grabResult)
+        {
+            converter.OutputPixelFormat = PixelType.BGR8packed;
+            byte[] buffer = grabResult.PixelData as byte[];
+            return new Mat(grabResult.Height, grabResult.Width, MatType.CV_8U, buffer);
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            CamInit();
         }
     }
 }
